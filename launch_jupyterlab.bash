bc26ea347652b2825c4e1ab12eb15a3e23bbc458
Enric Cervera <ecervera@uji.es>
2020-04-03 10:21:07 +0200

    test on Webots R2019

--- launch_jupyterlab.bash
+++ launch_jupyterlab.bash
@@ -2,9 +2,9 @@
 
 source iWebots-env/bin/activate
 
-export WEBOTS_HOME=${HOME}/webots-R2020a-rev1
-export LD_LIBRARY_PATH={LD_LIBRARY_PATH}:${WEBOTS_HOME}/lib/controller
-export PYTHONPATH=${PYTHONPATH}:${WEBOTS_HOME}/lib/controller/python36
+export WEBOTS_HOME=${HOME}/webots-R2019b-rev1
+export LD_LIBRARY_PATH={LD_LIBRARY_PATH}:${WEBOTS_HOME}/lib:${WEBOTS_HOME}/lib/controller
+export PYTHONPATH=${PYTHONPATH}:${WEBOTS_HOME}/lib/python36:${WEBOTS_HOME}/lib/controller/python36
 export PYTHONIOENCODING=UTF-8
 
 cd notebooks && jupyter lab

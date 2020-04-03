f9c18e56d302443857e830a1f786822ae1a657d8
Enric Cervera <ecervera@uji.es>
2020-04-03 14:18:41 +0200

    Fix variable

--- launch_jupyterlab.bash
+++ launch_jupyterlab.bash
@@ -3,7 +3,7 @@
 source WebotsLab-env/bin/activate
 
 export WEBOTS_HOME=${HOME}/webots-R2020a-rev1
-export LD_LIBRARY_PATH={LD_LIBRARY_PATH}:${WEBOTS_HOME}/lib:${WEBOTS_HOME}/lib/controller
+export LD_LIBRARY_PATH={$LD_LIBRARY_PATH}:${WEBOTS_HOME}/lib:${WEBOTS_HOME}/lib/controller
 export PYTHONPATH=${PYTHONPATH}:${WEBOTS_HOME}/lib/python36:${WEBOTS_HOME}/lib/controller/python36
 export PYTHONIOENCODING=UTF-8
 

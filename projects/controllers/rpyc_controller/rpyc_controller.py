f0b73ce93e5cdd70a918e5643b1aea32e221d377
Enric Cervera <ecervera@uji.es>
2020-04-06 18:12:51 +0200

    clean up code

--- projects/controllers/rpyc_controller/rpyc_controller.py
+++ projects/controllers/rpyc_controller/rpyc_controller.py
@@ -6,15 +6,6 @@ class RobotService(rpyc.Service):
     def __init__(self, robot):
         super().__init__()
         self.exposed_robot = robot
-        
-    def on_connect(self, conn):
-        pass
-
-    def on_disconnect(self, conn):
-        pass
-        
-    def restartSimulation(self):
-        self.exposed_robot.simulationReset()
     
 from controller import Robot
 from rpyc.utils.server import ThreadedServer
@@ -31,4 +22,4 @@ t.start()
 while robot.step(timestep) != -1:
     pass
 
-t.stop()
+t.close()

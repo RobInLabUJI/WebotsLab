07a017f69f95408f8dc4c79b4c330e5619f8430c
Enric Cervera <ecervera@uji.es>
2020-04-06 18:45:45 +0200

    add docker version

---
+++ launch_docker.bash
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+xhost +local:root
+
+docker run --rm -it \
+    --gpus all \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -p 8888:8888 \
+    webotslab
+
+xhost -local:root

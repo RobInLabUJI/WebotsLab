1e49dc43f6cad4dac4b2fe61def8dcf1be36e2a5
Enric Cervera <ecervera@uji.es>
2020-04-09 09:14:14 +0200

    cleanup native

--- install_jupyterlab.bash
+++ install_jupyterlab.bash
@@ -4,3 +4,9 @@ python3 -m venv WebotsLab-env
 source WebotsLab-env/bin/activate
 pip install jupyterlab rpyc
 
+mkdir projects
+cp -R controllers/ projects/
+mkdir projects/worlds
+cp worlds/r2020a/*.wbt projects/worlds/
+cp worlds/r2020a/.*.wbproj projects/worlds/
+
--- launch_jupyterlab.bash
+++ launch_jupyterlab.bash
@@ -2,7 +2,7 @@
 
 source WebotsLab-env/bin/activate
 
-export WEBOTS_HOME=${HOME}/webots-R2020a-rev1
+export WEBOTS_HOME=${HOME}/webots
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${WEBOTS_HOME}/lib:${WEBOTS_HOME}/lib/controller
 export PYTHONPATH=${PYTHONPATH}:${WEBOTS_HOME}/lib/python36:${WEBOTS_HOME}/lib/controller/python36
 export PYTHONIOENCODING=UTF-8

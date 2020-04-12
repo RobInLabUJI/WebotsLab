6b5fa21ceee6c828ff13efebc491209ffe70789e
Enric Cervera <ecervera@uji.es>
2020-04-12 10:48:21 +0200

    launch stream

--- install_WebotsLab.bash
+++ install_WebotsLab.bash
@@ -11,9 +11,9 @@ mkdir -p ${ENVDIR}/notebooks
 
 cp -R controllers/ ${ENVDIR}/projects/
 
-cp worlds/r2020a/*.wbt ${ENVDIR}/projects/worlds/
-cp worlds/r2020a/.*.wbproj ${ENVDIR}/projects/worlds/
+cp worlds/r2020a/pioneer3dx.wbt ${ENVDIR}/projects/worlds/
+cp worlds/r2020a/.pioneer3dx.wbproj ${ENVDIR}/projects/worlds/
 
-cp notebooks/test_py3.ipynb ${ENVDIR}/notebooks/test.ipynb
-cp notebooks/test_py3_extern_controller.ipynb ${ENVDIR}/notebooks/
+cp notebooks/test_py3_stream.ipynb ${ENVDIR}/notebooks/test.ipynb
 
+cp -R stream/ ${ENVDIR}

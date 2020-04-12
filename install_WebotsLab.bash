013ceba89cdca04c744949099ee947f7d552aeb1
Enric Cervera <ecervera@uji.es>
2020-04-12 11:26:58 +0200

    add khepera world

--- install_WebotsLab.bash
+++ install_WebotsLab.bash
@@ -13,6 +13,8 @@ cp -R controllers/ ${ENVDIR}/projects/
 
 cp worlds/$1/pioneer3dx.wbt ${ENVDIR}/projects/worlds/
 cp worlds/$1/.pioneer3dx.wbproj ${ENVDIR}/projects/worlds/
+cp worlds/$1/khepera4.wbt ${ENVDIR}/projects/worlds/
+cp worlds/$1/.khepera4.wbproj ${ENVDIR}/projects/worlds/
 
 cp notebooks/test_py3_stream.ipynb ${ENVDIR}/notebooks/test.ipynb
 

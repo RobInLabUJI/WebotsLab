041a3390c5d0deab70970b01f8218e09d295cadc
Enric Cervera <ecervera@uji.es>
2020-04-13 10:04:46 +0200

    use scripts in docker

---
+++ launch_WebotsLab_docker.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ENVDIR=./wlenv
+
+source ${ENVDIR}/bin/activate
+
+export WEBOTS_HOME=${HOME}/webots
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${WEBOTS_HOME}/lib:${WEBOTS_HOME}/lib/controller
+export PYTHONPATH=${PYTHONPATH}:${WEBOTS_HOME}/lib/python36:${WEBOTS_HOME}/lib/controller/python36
+export PYTHONIOENCODING=UTF-8
+
+cd ${ENVDIR}/stream && python3 server.py &
+SERVER_ID=$!
+
+cd ${ENVDIR} && ${WEBOTS_HOME}/webots --batch --stream --minimize ./projects/worlds/pioneer3dx.wbt  &
+WEBOTS_ID=$!
+
+cd ${ENVDIR} && jupyter lab --no-browser --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.notebook_dir='notebooks'
+
+for i in `ps -ef| awk '$3 == '$SERVER_ID' { print $2 }'`
+ do
+   echo killing $i
+   kill $i
+ done
+
+for i in `ps -ef| awk '$3 == '$WEBOTS_ID' { print $2 }'`
+ do
+   echo killing $i
+   kill $i
+ done

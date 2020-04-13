02752d2a6bfe4af7f72d24457738dd9df07ded70
Enric Cervera <ecervera@uji.es>
2020-04-13 10:22:22 +0200

    use Xvfb

--- launch_WebotsLab_docker.bash
+++ launch_WebotsLab_docker.bash
@@ -9,10 +9,16 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${WEBOTS_HOME}/lib:${WEBOTS_HOME}/lib/
 export PYTHONPATH=${PYTHONPATH}:${WEBOTS_HOME}/lib/python36:${WEBOTS_HOME}/lib/controller/python36
 export PYTHONIOENCODING=UTF-8
 
+export AUDIODEV=null
+export DEBIAN_FRONTEND=noninteractive
+export DISPLAY=:99
+export LIBGL_ALWAYS_SOFTWARE=true
+Xvfb :99 -screen 0 1024x768x16 &
+
 cd ${ENVDIR}/stream && python3 server.py &
 SERVER_ID=$!
 
-cd ${ENVDIR} && ${WEBOTS_HOME}/webots --batch --stream --minimize ./projects/worlds/pioneer3dx.wbt  &
+cd ${ENVDIR} && ${WEBOTS_HOME}/webots --stdout --stderr --no-sandbox --batch --stream --minimize ./projects/worlds/pioneer3dx.wbt  &
 WEBOTS_ID=$!
 
 cd ${ENVDIR} && jupyter lab --no-browser --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.notebook_dir='notebooks'

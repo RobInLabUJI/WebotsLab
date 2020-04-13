7b620048d5ebac1df7b79bd8f182bb7136382fbb
Enric Cervera <ecervera@uji.es>
2020-04-13 13:26:13 +0200

    remove X11 options

--- launch_docker.bash
+++ launch_docker.bash
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-xhost +local:root
-
 docker run --rm -it \
-    --gpus all \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -p 8888:8888 \
     -p 8889:8889 \
     -p 1234:1234 \
     webotslab:$1
 
-xhost -local:root

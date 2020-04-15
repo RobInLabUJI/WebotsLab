86f86fb6d2ee108adc6c822e5ff865f1b3d68d51
Enric Cervera <ecervera@uji.es>
2020-04-15 23:17:10 +0200

    use <extern> controller

--- launch_WebotsLab.bash
+++ launch_WebotsLab.bash
@@ -9,19 +9,10 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${WEBOTS_HOME}/lib:${WEBOTS_HOME}/lib/
 export PYTHONPATH=${PYTHONPATH}:${WEBOTS_HOME}/lib/python36:${WEBOTS_HOME}/lib/controller/python36
 export PYTHONIOENCODING=UTF-8
 
-cd ${ENVDIR}/stream && python3 server.py &
-SERVER_ID=$!
-
-cd ${ENVDIR} && ${WEBOTS_HOME}/webots --batch --stream --minimize ./projects/worlds/khepera4.wbt  &
+cd ${ENVDIR} && ${WEBOTS_HOME}/webots ./projects/worlds/khepera4.wbt  &
 WEBOTS_ID=$!
 
 cd ${ENVDIR} && jupyter lab --NotebookApp.notebook_dir='notebooks'
-
-for i in `ps -ef| awk '$3 == '$SERVER_ID' { print $2 }'`
- do
-   echo killing $i
-   kill $i
- done
 
 for i in `ps -ef| awk '$3 == '$WEBOTS_ID' { print $2 }'`
  do
--- notebooks/controller.py
+++
@@ -1,6 +0,0 @@
-import rpyc
-
-def Robot():
-    conn = rpyc.connect("localhost", 18861)
-    robot = conn.root.robot
-    return robot

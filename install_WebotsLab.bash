39f9ec446516311e0122a3c83314c9448277daed
Enric Cervera <ecervera@uji.es>
2020-04-17 10:59:21 +0200

    add opencv code

--- install_WebotsLab.bash
+++ install_WebotsLab.bash
@@ -4,7 +4,7 @@ ENVDIR=./wlenv
 
 python3 -m venv ${ENVDIR}
 source ${ENVDIR}/bin/activate
-pip install jupyterlab numpy matplotlib
+pip install jupyterlab numpy matplotlib opencv-python
 
 cp -R projects/  ${ENVDIR}/
 cp -R notebooks/ ${ENVDIR}/
